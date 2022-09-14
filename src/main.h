a70326aff7c1647ceb56c4610d4f47de12b41ae4
Anthony Panarello <jpk8g2@gmail.com>
2022-09-14 19:59:45 -0400

    Deleted unecessary files

--- src/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#define MEM_ERR "Cannot secure memory for buffer\n"
-#define MEM_ERR_CDE 0200
-
-#endif
--- src/misc.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MISC_H
-#define _MISC_H
-
-char *securebuf(int);
-char *concat_int(char *, int);
-
-#endif
