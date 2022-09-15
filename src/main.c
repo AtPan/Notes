567a4201f0585fa204a4f4fad3dfb97be3541be7
Anthony Panarello <jpk8g2@gmail.com>
2022-09-14 20:00:18 -0400

    Added list switch which displays all existing notes

--- src/main.c
+++ src/main.c
@@ -1,17 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include "action.h"
-#include "main.h"
-#include "misc.h"
-#include "parse.h"
+#include "notes.h"
 
 int main(int, char*[]);
 
 /* Set up buffers and flags for file names, editing, and class name */
 char *name, *class, edit = 1;
-u_int8_t vbose = 0, skip = 0;
+int vbose = 0, skip = 0;
 char *editorpath, *touchpath, *classdir, *timefile;
 
 int main(int argc, char *argv[]) {
@@ -60,12 +53,14 @@ int main(int argc, char *argv[]) {
 			case 'h': 
 				fprintf(stdout, "Note-organizing program that automatically sorts notes.\n" 
 				"Optional Flags:\n"
-				"\t-h: 			Shows this message\n"
-				"\t-c class:	Override automatic class schedule with a given class\n"
-				"\t-n name: 	Override automatice naming scheme with a custom file name\n"
-				"\t-e:			Only create the note file, do not open to edit\n"
-				"\t-v(v):		Have the program send more and more updates to stdout\n"
-				"\t-a:			Add a class to your schedule\n");
+				"\t-h:          Shows this message\n"
+				"\t-c class:    Override automatic class schedule with a given class\n"
+				"\t-n name:     Override automatice naming scheme with a custom file name\n"
+				"\t-e:          Only create the note file, do not open to edit\n"
+				"\t-v(v):       Have the program send more and more updates to stdout\n"
+				"\t-l:          List all saved notes under all stored class directories\n"
+//				"\t-d:          Delete a class from your schedule\n"
+				"\t-a:          Add a class to your schedule\n");
 				return 0;
 			case 'c':
 				/* Skips class name to the next potential arg */
@@ -98,12 +93,43 @@ int main(int argc, char *argv[]) {
 			case 'e': /* Turns off editing flag */
 				edit = 0;
 				break;
+			case 'l': /* List all notes */
+				action = &list_notes;
+				break;
 			case 'v': /* Sets verbose flag */
 				for(; *(argv[i] + vbose + 1) == 'v'; vbose++);
+				break;
+			case 'd':
+				action = &delete_class;
+				break;
+			default:
+				printf("Invalid switch passed\n");
+				exit(1);
 				break;
 		}
 	}
 
 	/* Pass execution to proper function */
 	(*action)();
+
+	return 0;
 }
+
+/*
+void delete_class() {
+	FILE *fp = fopen(timefile, "r+");
+	if(fp == NULL) exit(1);
+
+	char buf[256];
+	int i;
+
+	for(i = 1; fgets(buf, 256, fp) != NULL; i++) {
+		printf("%d. %s", i, buf);
+	}
+	fflush(stdout);
+	fseek(fp, 0, SEEK_SET);
+
+
+	fclose(fp);
+}
+*/
